65f32d915336b5cb58aec2556caa41861ec81a8f
Senryuugg <senryuugg@gmail.com>
2026-03-12 14:05:51 +0800

    Final V2

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header({ title, user }) {
           <div className="header-text">
             <p className="header-gov">REPUBLIC OF THE PHILIPPINES</p>
             <p className="header-dept">DEPARTMENT OF AGRICULTURE</p>
-            <p className="header-bureau">BUREAU OF FISHERIES AND AQUATIC RESOURCES - NCR</p>
+            <p className="header-bureau">BUREAU OF FISHERIES AND AQUATIC RESOURCES - NATIONAL CAPTIAL REGION</p>
             <p className="header-title">FISHERIES AND AQUATIC RESOURCES MANAGEMENT COUNCILS INFORMATION SYSTEM</p>
           </div>
         </div>
